f1d5d3a5fdfbe07669466b2199427e669cc6d441
Aliaksandr Autayeu <autayeu@users.sourceforge.net>
2010-02-04 22:33:44 +0100

    bundled with wordnet

--- bin/convert-cwTab2XML.sh
+++ bin/convert-cwTab2XML.sh
@@ -1,2 +1,2 @@
-java -Xmx256M -Xms256M -Dlog4j.configuration=../conf/log4j.properties -cp $CP it.unitn.disi.smatch.MatchManager convert ../test-data/cw/c.txt ../test-data/cw/c.xml -prop=../conf/SMatchTab2XMLL.properties
-java -Xmx256M -Xms256M -Dlog4j.configuration=../conf/log4j.properties -cp $CP it.unitn.disi.smatch.MatchManager convert ../test-data/cw/w.txt ../test-data/cw/w.xml -prop=../conf/SMatchTab2XMLL.properties
+java -Xmx256M -Xms256M -Dlog4j.configuration=../conf/log4j.properties -cp $CP it.unitn.disi.smatch.MatchManager convert ../test-data/cw/c.txt ../test-data/cw/c.xml -prop=../conf/SMatchTab2XML.properties
+java -Xmx256M -Xms256M -Dlog4j.configuration=../conf/log4j.properties -cp $CP it.unitn.disi.smatch.MatchManager convert ../test-data/cw/w.txt ../test-data/cw/w.xml -prop=../conf/SMatchTab2XML.properties
