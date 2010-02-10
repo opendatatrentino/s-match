c4eabca5c3a29977e208e3aa9b1aa2b7d743874e
Aliaksandr Autayeu <avtaev@gmail.com>
2010-02-10 22:32:20 +0100

    case sensitivity

--- bin/all-cw.sh
+++ bin/all-cw.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CP=../build/s-match.jar:../lib/JWNL/commons-logging.jar:../lib/JWNL/jwnl.jar:../lib/JWNL/tests.jar:../lib/JWNL/utilities.jar:../lib/orbital/orbital-core.jar:../lib/orbital/orbital-ext.jar:../lib/sat/jsat.jar:../lib/sat/minilearningbr.jar:../lib/sat/sat4j-1.5new.jar:../lib/xml/xercesImpl.jar:../lib/xml/xmlParserAPIs.jar:../lib/log4j/log4j-1.2.14.jar
+export CP=../build/s-match.jar:../lib/jwnl/commons-logging.jar:../lib/jwnl/jwnl.jar:../lib/jwnl/tests.jar:../lib/jwnl/utilities.jar:../lib/orbital/orbital-core.jar:../lib/orbital/orbital-ext.jar:../lib/sat/jsat.jar:../lib/sat/minilearningbr.jar:../lib/sat/sat4j-1.5new.jar:../lib/xml/xercesImpl.jar:../lib/xml/xmlParserAPIs.jar:../lib/log4j/log4j-1.2.14.jar
 
 . ./convert-cwTab2XML.sh
 . ./offline-cw.sh SMatchDefault
