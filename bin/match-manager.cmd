213247a7c064c2134e7833ab0edeb3a7656feb62
Aliaksandr Autayeu <avtaev@gmail.com>
2010-05-15 23:50:34 +0200

    log4j updated

--- bin/match-manager.cmd
+++ bin/match-manager.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set CP=..\build\s-match.jar;..\lib\JWNL\commons-logging.jar;..\lib\JWNL\jwnl.jar;..\lib\JWNL\tests.jar;..\lib\JWNL\utilities.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\jsat.jar;..\lib\sat\minilearningbr.jar;..\lib\sat\sat4j-1.5new.jar;..\lib\xml\xercesImpl.jar;..\lib\xml\xmlParserAPIs.jar;..\lib\log4j\log4j-1.2.14.jar
+set CP=..\build\s-match.jar;..\lib\JWNL\commons-logging.jar;..\lib\JWNL\jwnl.jar;..\lib\JWNL\tests.jar;..\lib\JWNL\utilities.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\jsat.jar;..\lib\sat\minilearningbr.jar;..\lib\sat\sat4j-1.5new.jar;..\lib\xml\xercesImpl.jar;..\lib\xml\xmlParserAPIs.jar;..\lib\log4j\log4j-1.2.16.jar
 
 java -Xmx256M -Xms256M -Dlog4j.configuration=..\conf\log4j.properties -cp %CP% it.unitn.disi.smatch.MatchManager %*
 
