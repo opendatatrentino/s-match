2b00eecc887f66adcfcf96c8bb993ccbd6333c24
Aliaksandr Autayeu <avtaev@gmail.com>
2011-01-20 17:09:12 +0100

    lib extjwnl updated

--- bin/match-manager.cmd
+++ bin/match-manager.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set CP=..\build\s-match.jar;..\lib\extjwnl\commons-logging.jar;..\lib\extjwnl\extjwnl-20110110.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\minilearning\minilearningbr.jar;..\lib\sat\SAT4J\org.sat4j.core.jar;..\lib\xml\xercesImpl.jar;..\lib\log4j\log4j-1.2.16.jar;..\lib\jgraph\jgrapht-0.6.0.jar;..\lib\hermit\HermiT.jar;..\lib\owlapi\owlapi-bin.jar;..\lib\skosapi\skosapi.jar
+set CP=..\build\s-match.jar;..\lib\extjwnl\commons-logging.jar;..\lib\extjwnl\extjwnl-20110117.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\minilearning\minilearningbr.jar;..\lib\sat\SAT4J\org.sat4j.core.jar;..\lib\xml\xercesImpl.jar;..\lib\log4j\log4j-1.2.16.jar;..\lib\jgraph\jgrapht-0.6.0.jar;..\lib\hermit\HermiT.jar;..\lib\owlapi\owlapi-bin.jar;..\lib\skosapi\skosapi.jar
 
 java -Xmx256M -Xms256M -Dlog4j.configuration=..\conf\log4j.properties -cp %CP% it.unitn.disi.smatch.MatchManager %*
 
--- bin/runGUI.cmd
+++ bin/runGUI.cmd
@@ -1,6 +1,6 @@
 @echo off
 setlocal
-set CP=..\build\s-match.jar;..\build\s-match-gui.jar;..\lib\extjwnl\commons-logging.jar;..\lib\extjwnl\extjwnl-20110110.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\minilearning\minilearningbr.jar;..\lib\sat\SAT4J\org.sat4j.core.jar;..\lib\xml\xercesImpl.jar;..\lib\log4j\log4j-1.2.16.jar;..\lib\jgraph\jgrapht-0.6.0.jar;..\lib\hermit\HermiT.jar;..\lib\owlapi\owlapi-bin.jar;..\lib\skosapi\skosapi.jar;..\lib\gui\ICOReader-1.04.jar;..\lib\jgoodies\jgoodies-common-1.0.0.jar;..\lib\jgoodies\jgoodies-forms-1.4.0b1.jar;..\lib\jic\java-icon.jar;..\lib\salamander\svgSalamander.jar
+set CP=..\build\s-match.jar;..\build\s-match-gui.jar;..\lib\extjwnl\commons-logging.jar;..\lib\extjwnl\extjwnl-20110117.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\minilearning\minilearningbr.jar;..\lib\sat\SAT4J\org.sat4j.core.jar;..\lib\xml\xercesImpl.jar;..\lib\log4j\log4j-1.2.16.jar;..\lib\jgraph\jgrapht-0.6.0.jar;..\lib\hermit\HermiT.jar;..\lib\owlapi\owlapi-bin.jar;..\lib\skosapi\skosapi.jar;..\lib\gui\ICOReader-1.04.jar;..\lib\jgoodies\jgoodies-common-1.0.0.jar;..\lib\jgoodies\jgoodies-forms-1.4.0b1.jar;..\lib\jic\java-icon.jar;..\lib\salamander\svgSalamander.jar
 
 start javaw -Xmx256M -Xms256M -Dlog4j.configuration=..\conf\log4j-gui.properties -cp %CP% it.unitn.disi.smatch.gui.SMatchGUI
 
