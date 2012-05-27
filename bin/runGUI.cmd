0c00f2357174cd3c06eaae44c99737e90bf53f9e
Aliaksandr Autayeu <aliaksandr@autayeu.com>
2012-05-27 20:14:26 +0200

    added classpath parameters to scripts

--- bin/runGUI.cmd
+++ bin/runGUI.cmd
@@ -1,6 +1,7 @@
 @echo off
+REM Use S-MATCH-CP to add your jar to classpath
 setlocal
-set CP=..\build\s-match.jar;..\build\s-match-gui.jar;..\lib\extjwnl\commons-logging-1.1.1.jar;..\lib\extjwnl\extjwnl-1.6.2.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\minilearning\minilearningbr.jar;..\lib\sat\SAT4J\org.sat4j.core.jar;..\lib\xml\xercesImpl.jar;..\lib\log4j\log4j-1.2.16.jar;..\lib\jgraph\jgrapht-0.6.0.jar;..\lib\hermit\HermiT.jar;..\lib\owlapi\owlapi-bin.jar;..\lib\skosapi\skosapi.jar;..\lib\gui\ICOReader-1.04.jar;..\lib\jgoodies\jgoodies-common-1.2.1.jar;..\lib\jgoodies\jgoodies-forms-1.4.2.jar;..\lib\jic\java-icon.jar;..\lib\salamander\svgSalamander.jar;..\lib\opennlp\opennlp-maxent-3.0.1-incubating.jar;..\lib\opennlp\opennlp-tools-1.5.1-incubating.jar
+set CP=..\build\s-match.jar;..\build\s-match-gui.jar;..\lib\extjwnl\commons-logging-1.1.1.jar;..\lib\extjwnl\extjwnl-1.6.2.jar;..\lib\orbital\orbital-core.jar;..\lib\orbital\orbital-ext.jar;..\lib\sat\minilearning\minilearningbr.jar;..\lib\sat\SAT4J\org.sat4j.core.jar;..\lib\xml\xercesImpl.jar;..\lib\log4j\log4j-1.2.16.jar;..\lib\jgraph\jgrapht-0.6.0.jar;..\lib\hermit\HermiT.jar;..\lib\owlapi\owlapi-bin.jar;..\lib\skosapi\skosapi.jar;..\lib\gui\ICOReader-1.04.jar;..\lib\jgoodies\jgoodies-common-1.2.1.jar;..\lib\jgoodies\jgoodies-forms-1.4.2.jar;..\lib\jic\java-icon.jar;..\lib\salamander\svgSalamander.jar;..\lib\opennlp\opennlp-maxent-3.0.1-incubating.jar;..\lib\opennlp\opennlp-tools-1.5.1-incubating.jar;%S-MATCH-CP%
 
 start javaw -Xmx256M -Xms256M -Dlog4j.configuration=..\conf\log4j-gui.properties -cp %CP% it.unitn.disi.smatch.gui.SMatchGUI
 
